fd3bb5a607c0d5083da0ccbc88b6c67f24ebaa73
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2022-05-22 09:36:26 -0600

    Added some notes on python
    trying out hello world, variables, type conversions, and input methods using python

---
+++ python_test/main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print('Hello Friend')
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('World')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+age = 25
+
+print(age)
+
+year = 2022
+myAge = int(input('How old are you? '))
+birth_year = year - myAge
+print('You were born in ' + str(birth_year))
