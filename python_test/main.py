98f17e969d1eea8815b20d42e12f905527f41851
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2022-05-22 17:01:53 -0600

    Update main.py
    How to use print(f) with the .upper() and .lower() methods to force upper case or lower case (string manipulation)

--- python_test/main.py
+++ python_test/main.py
@@ -21,10 +21,10 @@ age = 25
 
 print(age)
 
-year = 2022
-myAge = int(input('How old are you? '))
-birth_year = year - myAge
-print('You were born in ' + str(birth_year))
+# year = 2022
+# myAge = int(input('How old are you? '))
+# birth_year = year - myAge
+# print('You were born in ' + str(birth_year))
 
 
 print("My name is Jim and it's nice to meet you!")
@@ -34,4 +34,7 @@ start_time = 10
 type = 'AM'
 name = 'John'
 
-print(f'{name} is a loser, he wakes up at {start_time}{type} and only runs for {distance_travelled} kilometers ')
+print(f'{name} is a winner, he wakes up at {start_time}{type} and actually runs for {distance_travelled} kilometers ')
+
+# .upper() method forces all caps, .lower() forces all lower case
+print(f'{name.upper()} is a winner, he wakes up at {start_time}{type.lower()} and actually runs for {distance_travelled} kilometers ')
